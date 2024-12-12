3f255d8cfb1928eda47ac543c9c60b8cc9a69e86
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2024-12-12 18:39:15 -0300

    feat: se agrega mejora

--- src/core/state/store.config.ts
+++ src/core/state/store.config.ts
@@ -5,8 +5,7 @@ import { storeLogger } from '../middlewares/store-logger.middleware'
 import { rootReducer } from './reducer.config'
 
 /**
- * Configures and initializes the Redux store.
- * This function sets up the store with middleware, devTools, and the root reducer.
+ * Configures and initializes the store.
  */
 export const store = configureStore({
   devTools: ENVIRONMENT !== 'prod',
